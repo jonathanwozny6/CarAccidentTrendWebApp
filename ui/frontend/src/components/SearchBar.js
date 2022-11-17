acda3f4538801b0b969686dee30bffcf6849dcb8
Alexander Wozny <alexanderwozny24@gmail.com>
2022-11-16 21:53:12 -0500

    Updated SearchBar.jss so that clicking closes the menu. Cleaned up code and commented nicely too

--- ui/frontend/src/components/SearchBar.js
+++ ui/frontend/src/components/SearchBar.js
@@ -4,55 +4,94 @@ import {useEffect, useState} from 'react';
 import "./SearchBar.css";
 
 function SearchBar({placeholder, data, childToParent}) {
-    const [dataFiltered, setDataFiltered] = useState(data.map(item => {return item}));
-    const [filterOpen, setFilterOpen] = useState(false);
-    const [firstOpen, setFirstOpen] = useState(true);
-    const [resBoxHeight, setResBoxHeight] = useState(120);
-    const [selection, setSelection] = useState("");
+    // stores list of filtered data
+    const [dataFiltered, setDataFiltered] = useState(data.map(item => {return item.value}));
 
+    // state of whether the dropdown is open or not
+    const [filterOpen, setFilterOpen] = useState(false);
+
+    // height in pixels of the dropdown display
+    const [resBoxHeight, setResBoxHeight] = useState(120);
+
+    // currently selected item
+    const [selection, setSelection] = useState(placeholder);
+
+    // current input in search box
+    const [currSearch, setCurrSearch] = useState(1);
+
+    // number of elements to display in dropdown
     const numDisplay = 5;
 
+    // function for display of dropdown on user text input
     const FilterSearch = (event) => {
-        const currSearch = event.target.value;
+
+        // set the current search to the user input
+        setCurrSearch(event.target.value);
+
+        // get the current filtered data
         const currFilter = data.filter((item) => {
             return item.value.toLowerCase().startsWith(currSearch.toLowerCase());
         });
 
+
+        // if the search bar is empty and dropdown closed
         if (currSearch === "" && filterOpen===false){
+          // filtered data is empty
           setDataFiltered([]);
+
+          // initialize display so that it will show at most 5 elements at once
           setResBoxHeight(numDisplay*20)
         }
-        else if(currSearch ==="" && filterOpen===true && firstOpen===true){
+        // if the search bar is empty and dropdown open
+        else if(currSearch ==="" && filterOpen===true){
+          // show all options
           const allItems = data.map((item) => {
             return item;
           })
           setDataFiltered(allItems);
-        }
-        else if (currSearch !== "" && filterOpen === false){
-          setFilterOpen(true)
-          setDataFiltered(currFilter);
-          setResBoxHeight(Math.min(dataFiltered.length*20, numDisplay*20))
-        }
-        else if (currSearch !== ""){
-          setFilterOpen(true)
-          setDataFiltered(currFilter);
-          setFirstOpen(false)
-          setResBoxHeight(Math.min(dataFiltered.length*20, numDisplay*20))
-        }
-        else{
-          setFilterOpen(false)
-          setFirstOpen(false)
-          setDataFiltered([])
+
+          // set display to show at most 5 elements at once
           setResBoxHeight(numDisplay*20)
+        }
+        // if the search bar is not empty and dropdown is not open
+        else if (currSearch !== "" && filterOpen === false){
+          // open the dropdown
+          setFilterOpen(true)
+
+          // only show filtered data
+          setDataFiltered(currFilter);
+
+          // set the dropdown to show either the specific maximum value or the filtered values if lower
+          setResBoxHeight(Math.min(dataFiltered.length*20, numDisplay*20))
+        }
+        // if the search bar is not empty and the dropdown is open
+        else if (currSearch !== "" && filterOpen === true){
+          // set dropdown to open
+          setFilterOpen(true)
+
+          // only show filtered data
+          setDataFiltered(currFilter);
+
+          // set the dropdown to show either the specific maximum value or the filtered values if lower
+          setResBoxHeight(Math.min(dataFiltered.length*20, numDisplay*20))
         }
     }
 
+    // function to open dropdown on click
     const OpenFilter = () => {
+        // open/close filter
         setFilterOpen(!filterOpen);   
+
+        // set filtered data to display all entries
         setDataFiltered(data.map(item => {return item}))    
     }
 
+    // function to make selection on click
     const makeSelection = (event) => {
+        // close dropdown once selection is made
+        setCurrSearch("")
+        setFilterOpen(false)
+
         // get inner text of div element (full state name) and set selection
         setSelection(event.target.innerText);
 
@@ -63,13 +102,15 @@ function SearchBar({placeholder, data, childToParent}) {
                 code = value.code;
             }
         }
+
+        // send data to parent
         childToParent(code);
     }
 
     return (
         <div className="search">
             <div className="input-container">
-                <input className="search-input" type="text" placeholder={placeholder} onChange={FilterSearch}/>
+                <input className="search-input" type="text" placeholder={selection} onChange={FilterSearch}/>
                 <div className="down-arrow" onClick={OpenFilter}>
                   <svg width="20" height="20">
                     <polygon points="4,7 12,7 8,11"/>
@@ -84,8 +125,6 @@ function SearchBar({placeholder, data, childToParent}) {
             </div>
             )}
         </div>
-        
-        
     )
 }
 
