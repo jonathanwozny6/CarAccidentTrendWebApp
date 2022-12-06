f9ce73b446d3111abb1fa2af6b8b66d2d5690eef
Alexander Wozny <alexanderwozny24@gmail.com>
2022-12-06 01:51:08 -0500

    Initial commit for Query1

--- ui/frontend/src/pages/Query1.js
+++ ui/frontend/src/pages/Query1.js
@@ -1,30 +1,226 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import SearchBar from '../components/SearchBar';
+import DateInput from '../components/DateInput'
+import dataStates from "../ParamData/States.json"
+import "./PagesCSS/Query1.css"
+import axios from "axios";
+import {
+    LineChart,
+    ResponsiveContainer,
+    Legend, Tooltip,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid
+} from 'recharts';
+import * as scale from 'd3-scale'
 
-const Query1 = props => {
-    const [data, setData] = React.useState([]);
-    React.useEffect(() => {
-        axios.get('http://localhost:8080/query1?state1=NY')
-        .then(response=>{
-            if (response.status===200) {
-                const fetchedData = response.data.rows;
-                console.log('fetchedData', fetchedData.length);
-                setData(fetchedData)
-            }
-            console.log("successfully retrieved data for query 1")
-        })
-        .catch(err => {
-            console.log("error in getting data from query1")
-        })
-    }, []);
-    return (<div>
-        Query1 Component
-        {
-            data.length>0 && 
-            data.map((item, index) =>
-                <div key={index}> {item[0]} - {item[1]} -  {item[2]} -  {item[3]} -  {item[4]} </div>)
+const Query1 = () => {
+
+    // all data
+    var myData = {}
+
+    // counts the number of times the button is clicked
+    const [btnClickCnt, setBtnClickCnt] = useState(0);
+
+    const [stateLeg, setStateLeg] = useState([]);
+
+    // number of locations on the line plot
+    const [numLocs, setNumLocs] = useState(1);
+
+    // moving average window size
+    const [maWindow, setMAWindow] = useState(1);
+    
+    // maximum moving average window
+    const maxMA = 100;
+
+	// US State selected from Search bar dropdown
+	const [stateUS, setStateUS] = useState([])
+	// let stateUS = ["Enter a State..."]
+
+	// start and end date input
+	const [startDate, setStartDate] = useState("");
+	const [endDate, setEndDate] = useState("");
+
+		// function to pass into Search bar dropdown to get receive user input
+		const childToParent = (childSelectedState, index) => {
+				let oldStateUS = stateUS;
+				oldStateUS[index] = childSelectedState;
+				setStateUS(oldStateUS);
+		}
+
+	// function to pass into start date input to receive user input
+	const getStartDate = (startDateInput) => {
+			setStartDate(startDateInput);
+	}
+
+	// function to pass into end date input to receive user input
+	const getEndDate = (endDateInput) => {
+			setEndDate(endDateInput);
+	}
+
+	// function to add line when "Add Line" button is clicked
+	const addLineClicked = () => {
+        let oldStateUS = stateUS
+        setStateUS(oldStateUS.concat("Enter a State..."));
+	}
+
+    // parses the moving average input
+    const MovingAvgInput = (event) => {
+        const dummy = event.target.value;
+        if (parseInt(dummy) > maxMA)
+            event.target.value = dummy.slice(0, -1);
+        else if (parseInt(dummy) < 1)
+            event.target.value = dummy.slice(0, -1);
+
+        setMAWindow(event.target.value);
+    }
+
+    // function to plot line when plot button is clicked
+    const PlotLine = () => {
+        if (btnClickCnt === Number.MAX_SAFE_INTEGER){
+            setBtnClickCnt(1)
         }
-    </div>)
-}
+        else
+            setBtnClickCnt(btnClickCnt+1)
+    }
+
+	const [data, setData] = React.useState();
+
+    useEffect(() => {
+        const optionsDates = {
+            method: 'GET',
+            url: `http://localhost:8080/dates`, 
+            params: {sDate: startDate, eDate: endDate},
+        }
+
+        axios.request(optionsDates).then((response) => {
+            if (response.status===200) {
+                const fetchedData = response.data;
+                setData(fetchedData);
+            }
+        }).catch((error) => {
+            console.error(error)
+        })
+
+    }, [endDate])
+
+
+    useEffect(() => {
+        if (stateUS[0] != "Enter a State...") {
+
+        
+            const i = stateUS.slice(0,-1).length - 1
+
+            // options for data request to backend
+            const options = {
+                method: 'GET',
+                url: `http://localhost:8080/query1/${stateUS[i]}`, 
+                params: {sDate: startDate, eDate: endDate},
+            }
+
+            axios.request(options).then((response) => {
+                
+                if (response.status===200) {
+                    const fetchedData = response.data;
+                    console.log('fetchedData', fetchedData.length, fetchedData);
+                    myData = data
+                    console.log(myData)
+
+                    for (let k = 0; k < myData.length; k++) {
+                        myData[k][`${stateUS[i]}`] = null
+                    }
+
+                    let j = 0;
+                    for (let k = 0; k < fetchedData.length; k++) {
+                        while (j < myData.length && fetchedData[k]["ACC_DATE"] != myData[j]["ACC_DATE"]) {
+                            j = j + 1
+                        }
+                        myData[j][`${stateUS[i]}`] = fetchedData[k][`${stateUS[i]}`]
+                    }
+                    
+                    setData(myData)
+                    
+                    console.log("My Data", myData)
+                }
+                
+            }).catch((error) => {
+                console.error(error)
+            })
+        }
+        console.log("stateUS length", stateUS.length)
+        console.log("StateUS", stateUS)
+        console.log("State Leg", stateLeg)
+        
+    }, [btnClickCnt]);
+
+
+    // // https://codesandbox.io/s/81u1y?file=/src/App.js
+    const getRandomColor = () => {
+        return "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
+    };
+    
+    return (
+        <div className="page-container">
+            <h1>Average Accident Frequency by Region and Time Period</h1> 
+            <div className="display-container">
+                <div className="input-pnl">
+                        <div className="input-location-section">
+                                <h3 className='input-pnl-heading'>Location</h3>
+                                <div className="dropdown">
+                                        {
+                                            stateUS.map((state, index) => {
+                                                    return <SearchBar placeholder={"Enter a State..."} data={dataStates} childToParent={childToParent} index={index}/>
+                                            })
+                                        }
+                                </div>
+                                <button className="add-curve-btn" onClick={addLineClicked}>
+                                        Add Line +
+                                </button>
+                        </div>
+                        <DateInput header="Start Date" placeholder="YYYY/MM/DD" childToParent={getStartDate}/>
+                        <DateInput header="End Date" placeholder="YYYY/MM/DD" childToParent={getEndDate}/>
+                        <h3 className="input-pnl-heading" id="ma-heading">Moving Average Window</h3>
+                        <div className="moving-average-section">
+                            <p></p> <p></p>
+                            <p>1</p> 
+                            <p>&le;</p>
+                            <input id="ma-window" onChange={MovingAvgInput}/>
+                            <p> &le;</p> <p>{maxMA}</p>
+                            <p></p> <p></p>
+                        </div>
+                        <div className="center">
+                            <button id="plot-btn" onClick={PlotLine}><h3>P L O T !</h3></button>
+                        </div>
+                        
+                </div>
+                <div className="lineplot">
+                    <h1 className="text-heading">
+                    </h1>
+                    <ResponsiveContainer width="100%" aspect={2} >
+                        <LineChart data = {data} xScale={scale.scaleTime} options={{ maintainAspectRatio: false }} margin={{ right: 300 }}>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="ACC_DATE" numberOfTicks={6} />
+                            <YAxis></YAxis>
+                            <Legend />
+                            <Tooltip />
+                            {
+                                stateUS.slice(0,-1).map(st => {
+                                    return <Line
+                                    dataKey={`${st}`}
+                                    stroke={getRandomColor()} activeDot={{ r: 8 }}
+                                    connectNulls
+                                    />
+                                })
+                            }
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Query1;
+
