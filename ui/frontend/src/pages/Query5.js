aaa41436990791c8ec2c2e295ab36cbcbe63fbcc
Jonathan Wozny <jonathanwozny6@gmail.com>
2022-12-06 12:15:53 -0500

    radial inputs for years for query 5

--- ui/frontend/src/pages/Query5.js
+++ ui/frontend/src/pages/Query5.js
@@ -29,16 +29,16 @@ const Query5 = () => {
 
   const [stateLeg, setStateLeg] = useState([]);
 
-  // number of locations on the line plot
-  const [numLocs, setNumLocs] = useState(1);
-
   // US State selected from Search bar dropdown
   const [stateUS, setStateUS] = useState("Enter a State")
   // let stateUS = ["Enter a State..."]
 
   // start and end date input
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+//   const [startDate, setStartDate] = useState("");
+//   const [endDate, setEndDate] = useState("");
+
+  // year to look at
+  const [yr, setYr] = useState("")
 
   // button to plot line
   const [btnClickCnt, setBtnClickCnt] = useState(0);
@@ -47,27 +47,23 @@ const Query5 = () => {
   const childToParent = (childSelectedState) => {
 	setStateUS(childSelectedState);
 
-		
-	for (let i = 0; i < 12; i++) {
-		var dict = {}
-		dict['ACC_DATE'] = i+1
-		months.push(dict)
-	}
+	// for (let i = 0; i < 12; i++) {
+	// 	var dict = {}
+	// 	dict['ACC_DATE'] = i+1
+	// 	months.push(dict)
+	// }
 
-	console.log("months", months)
+	console.log("ctP months", months)
 	setData(months)
-	console.log(data)
+	console.log("ctp init data", data)
 }
 
-  // function to pass into start date input to receive user input
-  const getStartDate = (startDateInput) => {
-      setStartDate(startDateInput);
-  }
-
-  // function to pass into end date input to receive user input
-  const getEndDate = (endDateInput) => {
-      setEndDate(endDateInput);
-  }
+  // function to pass into Search bar dropdown hour granularity to receive user input
+  const GetTimeGranularity = (event) => {
+	const index = event.target.id
+	setYr("20" + index)
+	console.log("Year", yr)
+}
 
   // function to plot line when plot button is clicked
   const PlotLine = () => {
@@ -79,9 +75,6 @@ const Query5 = () => {
       else
           setBtnClickCnt(btnClickCnt+1)
 	console.log("State 1:", stateUS)
-
-	
-
   }
 
 
@@ -92,13 +85,14 @@ const Query5 = () => {
 			months.push(dict)
 		}
 
-		console.log("months", months)
+		console.log("Use effect months", months)
 		setData(months)
-		console.log(data)
+		console.log("Use effect Initialized data", data)
     }, [stateUS])
 
 
     useEffect(() => {
+		console.log("btn click", btnClickCnt)
 		for (let i = 0; i < w_cond.length; i++) {
 		
 			if (stateUS[0] != "Enter a State...") {
@@ -107,10 +101,10 @@ const Query5 = () => {
 				const options = {
 				method: 'GET',
 				url: `http://localhost:8080/query5/${stateUS}`, 
-				params: {sDate: startDate, eDate: endDate, w_cond: w_cond[i]},
+				params: {yr: yr, w_cond: w_cond[i]},
 				}
 
-				if (btnClickCnt % 2 == 0) {
+				if (btnClickCnt % 2 == 1 & btnClickCnt !== 0) {
 					axios.request(options).then((response) => {
 					
 					if (response.status===200) {
@@ -126,9 +120,7 @@ const Query5 = () => {
 						for (let k = 0; k < fetchedData.length; k++) {
 						while (j < myData.length && fetchedData[k]["ACC_DATE"] != myData[j]["ACC_DATE"]) {
 							j = j + 1
-							console.log("fetched index: ", fetchedData[k]["ACC_DATE"], "my Date ind", myData[j]["ACC_DATE"])
 						}
-						console.log("my date equals: ", myData[j][`${w_cond[i]}`], "fetched data equals: ", fetchedData[k][`${w_cond[i]}`]  )
 						myData[j][`${w_cond[i]}`] = fetchedData[k][`NORM_CNT`]
 						}
 						
@@ -166,10 +158,41 @@ const Query5 = () => {
 						<SearchBar placeholder={"Enter a State..."} data={dataStates} childToParent={childToParent} index={0}/>
                     </div>
                 </div>
-                <DateInput header="Start Date" placeholder="YYYY/MM/DD" childToParent={getStartDate}/>
-                <DateInput header="End Date" placeholder="YYYY/MM/DD" childToParent={getEndDate}/>
-                <div className="center">
-                  <button id="plot-btn" onClick={PlotLine}><h3>P L O T !</h3></button>
+                {/* <DateInput header="Start Date" placeholder="YYYY/MM/DD" childToParent={getStartDate}/>
+                <DateInput header="End Date" placeholder="YYYY/MM/DD" childToParent={getEndDate}/> */}
+                <div className="hour-granularity-selection-container"> 
+										<h3 className="input-pnl-heading">Year</h3>
+										<div className="hour-granularity-selection">
+											<div className="hour-granularity-input-container">
+											<div className="hour-granularity-radio">
+													<p>16</p>
+													<input type="radio" id="16" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+												<div className="hour-granularity-radio">
+													<p>17</p>
+													<input type="radio" id="17" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+												<div className="hour-granularity-radio">
+													<p>18</p>
+													<input type="radio" id="18" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+												<div className="hour-granularity-radio">
+													<p>19</p>
+													<input type="radio" id="19" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+												<div className="hour-granularity-radio">
+													<p>20</p>
+													<input type="radio" id="20" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+												<div className="hour-granularity-radio">
+													<p>21</p>
+													<input type="radio" id="21" name="time-granularity" onChange={GetTimeGranularity}/>
+												</div>
+											</div>
+										</div>
+									</div>
+				<div className="center">
+                  <button id="plot-btn" onClick={PlotLine}><h3>Plot Line</h3></button>
                 </div>
             </div>
             <div className="lineplot">
